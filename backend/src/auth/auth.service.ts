4a9998b022517774074c54cbd3bdf7860d8f0af4
Paulo Henrique PErei <74430831+paulofreyyy@users.noreply.github.com>
2025-04-23 18:23:13 -0300

    feat(backend): criando autenticação JWT

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,17 +1,23 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
-    constructor(private userService: UserService){}
+    constructor(
+        private userService: UserService,
+        private jwtService: JwtService,
+    ){}
 
-    async signIn(username: string, pass: string): Promise<any>{
+    async signIn(username: string, pass: string): Promise<{access_token: string}>{
         const user = await this.userService.findOne(username);
         if(user?.password !== pass) throw new UnauthorizedException()
 
-        const {password, ...result} = user;
+        const payload = {sub: user.userId, username: user.username}
         // TODO: Gerar JWT e retornar aqui
 
-        return result
+        return {
+            access_token: await this.jwtService.signAsync(payload)
+        }
     }
 }
