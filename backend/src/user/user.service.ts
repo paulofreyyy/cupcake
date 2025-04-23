ab7f57c28675164523973e24787aef5131f7ba08
Paulo Henrique PErei <74430831+paulofreyyy@users.noreply.github.com>
2025-04-23 18:15:12 -0300

    feat(backend): criando Sign In endpoint

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
 
+export type User = any;
+
 @Injectable()
-export class UserService {}
+export class UserService {
+    private readonly users =[
+        {
+            userId: 1,
+            username: 'paulo',
+            password: '123qwe'
+        }
+    ]
+
+    async findOne(username: string): Promise<User | undefined>{
+        return this.users.find(user => user.username === username)
+    }
+}
