201bada90375672a133eb16bfeae1c2a84b9bcb2
Paulo Henrique PErei <74430831+paulofreyyy@users.noreply.github.com>
2025-05-05 18:54:48 -0300

    feat(frontend): criando tela de registro

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AuthGuard } from './auth.guard';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent, canActivate: [AuthGuard]},
-    {path: 'login', component: LoginComponent}
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent}
 ]
--- frontend/src/app/pages/register/register.component.ts
+++ frontend/src/app/pages/register/register.component.ts
@@ -1,16 +1,18 @@
+
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
+    selector: 'app-register',
     standalone: true,
-    selector: 'app-login',
     imports: [
         CommonModule,
         FormsModule,
@@ -19,22 +21,21 @@ import { MatCardModule } from '@angular/material/card';
         MatButtonModule,
         MatCardModule
     ],
-    templateUrl: './login.component.html',
-    styleUrl: './login.component.css'
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css']
 })
-export class LoginComponent {
+export class RegisterComponent {
     private auth = inject(AuthService);
     public router = inject(Router);
 
+    name = '';
     email = '';
     password = '';
 
     submit() {
-        this.auth.login({ email: this.email, password: this.password }).subscribe({
-            next: (res) => {
-                this.router.navigate(['/']);
-            },
-            error: (err) => alert('Credenciais inválidas')
+        this.auth.register({ name: this.name, email: this.email, password: this.password }).subscribe({
+            next: () => this.router.navigate(['/login']),
+            error: () => alert('Erro ao registrar. Tente novamente.')
         });
     }
 }
