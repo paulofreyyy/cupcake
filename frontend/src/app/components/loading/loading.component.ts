bfea39eca9ad900f3c2190d677e66ffdbebca64a
Paulo Henrique PErei <74430831+paulofreyyy@users.noreply.github.com>
2025-05-21 19:20:59 -0300

    style(frontend): ajustando estilo do loading

--- frontend/src/app/components/loading/loading.component.ts
+++ frontend/src/app/components/loading/loading.component.ts
@@ -1,12 +1,13 @@
 import { Component, inject } from "@angular/core";
 import { LoadingHelper } from "../../shared/helpers/loading.helper";
 import { CommonModule } from "@angular/common";
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
     selector: 'app-loading',
     templateUrl: './loading.component.html',
-    styleUrls:  ['./loading.component.css'],
-    imports: [CommonModule]
+    styleUrls: ['./loading.component.css'],
+    imports: [CommonModule, MatProgressSpinnerModule]
 })
 export class LoadingComponent {
     private loadingHelper = inject(LoadingHelper)
